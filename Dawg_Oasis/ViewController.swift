d4e125f755f047d9c283083fe7d53dc79c61313e
rachdk <rachael.d.kim@hotmail.com>
2020-05-25 20:26:13 -0700

    Initial Commit

---
+++ Dawg_Oasis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dawg_Oasis
+//
+//  Created by Rachael Kim on 5/25/20.
+//  Copyright © 2020 Rachael Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
